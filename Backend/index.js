161a22ccf102b2b5c8d1828aa4252d64a9c1d302
saiyam rajput <141070294+Rajputsaiyam@users.noreply.github.com>
2025-07-02 13:45:41 +0530

    c

--- Backend/index.js
+++ Backend/index.js
@@ -1,22 +1,47 @@
-const http = require('http');
-const expressApplication = require('./app');
+// const http = require('http');
+// const expressApplication = require('./app');
+// const connectMongoDB = require('./models/index');
+// const PORT=process.env.PORT ?? 8000;
+// require('dotenv').config(); 
+
+// const uri = process.env.MONGO_URI;
+
+// async function init(){
+//     try{
+//         await connectMongoDB(process.env.MONGODB_URI);
+//         console.log("Mongo db is connected");
+//         const server = http.createServer(expressApplication);
+
+//         server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+//     }catch(err){
+//         console.error('Error at starting server ', err);
+//         process.exit(1);
+//     }
+// }
+
+// init();
+
+
+const app = require('./app');
 const connectMongoDB = require('./models/index');
-const PORT=process.env.PORT ?? 8000;
-require('dotenv').config(); 
+const serverless = require('serverless-http');
+require('dotenv').config();
 
-const uri = process.env.MONGO_URI;
+const uri = process.env.MONGODB_URI;
 
-async function init(){
-    try{
-        await connectMongoDB(process.env.MONGODB_URI);
-        console.log("Mongo db is connected");
-        const server = http.createServer(expressApplication);
+let isConnected = false;
 
-        server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-    }catch(err){
-        console.error('Error at starting server ', err);
-        process.exit(1);
-    }
+async function connectDBOnce() {
+  if (!isConnected) {
+    await connectMongoDB(uri);
+    isConnected = true;
+    console.log("MongoDB connected");
+  }
 }
 
-init();
+const handler = async (req, res) => {
+  await connectDBOnce(); // Ensure DB is connected before each request
+  return serverless(app)(req, res);
+};
+
+module.exports = handler;
